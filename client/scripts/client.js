af2308d38c3e518672c51f5267a7e20402ed8755
Mark Rothermal <rothermal2@comcast.net>
2016-11-05 23:09:16 -0500

    working with parameters

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -14,20 +14,20 @@ $(document).ready(function(){
 function init(){
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1 , 10000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1 , 1000);
     camera.position.z = 1000;
 
-    geometry = new THREE.BoxGeometry(500, 500, 500);
+    geometry = new THREE.BoxGeometry(100, 100, 100);
     material = new THREE.MeshBasicMaterial({color:0xff0000});
 
     mesh = new THREE.Mesh (geometry, material);
     scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(800, 450);
 
-    //document.body.appendChild(renderer.domElement);
-    $('#world').append(rederer);
+    document.body.appendChild(renderer.domElement);
+    //$('.world').append(renderer);
 }
 
 function animate(){
